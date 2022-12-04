f1227de1112ae81312e83a88df8818687cf9c936
unknown <oliwerklauze22@gmail.com>
2022-12-04 15:46:13 +0100

    day one done

--- Day1/script.py
+++ Day1/script.py
@@ -1,1 +1,18 @@
-test
+text = open('input.txt','r').readlines()
+
+totalCalories = []
+elvCalories = []
+for line in text:
+    if line=='\n':
+        totalCalories.append(sum(elvCalories))
+        elvCalories=[]
+    else:
+        elvCalories.append(int(line))
+totalCalories.append(sum(elvCalories))
+elvCalories=[]
+
+#first task
+print(max(totalCalories))
+
+#second task
+print(sum(sorted(totalCalories,reverse=True)[:3]))
