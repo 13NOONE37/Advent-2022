395224a10f190a74bf6979542d27ea89974f7cd9
unknown <oliwerklauze22@gmail.com>
2022-12-09 12:52:49 +0100

    day 8 done

--- Day 8/script.py
+++ Day 8/script.py
@@ -2,7 +2,9 @@ with open('input.txt','r') as file_input:
     lines = file_input.readlines()
     index = len(lines)
     visible=0
-
+    highestScore=1
+    
+    #part1
     for x in range(0,index):
         for y in range(0,index):
             tree = int(lines[y][x])
@@ -31,4 +33,46 @@ with open('input.txt','r') as file_input:
             if len(leftValues)==0 or len(rightValues)==0 or len(topValues)==0 or len(bottomValues)==0:
                visible+=1
 
+    #part2
+    for x in range(0,index):
+        for y in range(0,index):
+            tree = int(lines[y][x])
+            leftValues=[]
+            rightValues=[]
+            topValues=[]
+            bottomValues=[]
+
+            #top
+            for i in range(y,0,-1):
+                currentTree = int(lines[i-1][x])
+                topValues.append(currentTree)
+                if  currentTree>=tree:
+                    break      
+            #bottom
+            for i in range(y+1, index):
+                currentTree = int(lines[i][x])
+                bottomValues.append(currentTree)
+                if  currentTree>=tree:
+                    break  
+            #left
+            for i in range(x-1,-1,-1):
+                currentTree = int(lines[y][i])
+                leftValues.append(currentTree)
+                if  currentTree>=tree:
+                    break   
+            #right
+            for i in range(x+1, index):
+                currentTree = int(lines[y][i])
+                rightValues.append(currentTree)
+                if  currentTree>=tree:
+                    break   
+               
+            scenicScore = len(topValues)*len(bottomValues)*len(leftValues)*len(rightValues)
+            if scenicScore>highestScore:
+                highestScore=scenicScore
+            
+
+          
     print(f"Visible: {visible}")
+    print(f"HighestScore: {highestScore}")
+
